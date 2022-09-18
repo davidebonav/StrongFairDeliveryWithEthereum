c0277d69067063638c12eb80f0fe7ce824b1f279
Remix Workspace <>
2022-09-18 17:28:17 +0200

    implemented smart contract

--- contracts/FairDelivery.sol
+++ contracts/FairDelivery.sol
@@ -5,22 +5,84 @@ import "./Ownable.sol";
 import "./IFairDelivery.sol";
 
 contract FairDelivery is Ownable, IFairDelivery{
-
-    uint256 private constant MAX_INT = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
-
-    mapping (address => uint256) private currentLabel;
-    
-    function sendNonRepudiationOfOriginFlag (string calldata h_name, HASH hash ) public returns (bool) {
-
-        require(
-            currentLabel[msg.sender] < MAX_INT,
-            "ERRORE"
-        );
-
-        // emit 
-
-        
-
-        return true;
+    // -- Types --
+    struct StateData {
+        bytes32 proofToDo;
+        EmailState state;
     }
+
+    // -- Attributes --
+    uint256 private constant MAX_INT = 2**256-1;
+
+    mapping(address => uint256) public override currentLabel;
+    mapping(address => mapping(uint256 => StateData) ) private currentEmailState;
+
+    // -- Modifiers --
+    modifier labelsAvailable(){
+        if (currentLabel[msg.sender] == MAX_INT)
+            revert NotEnoughLabels(
+                msg.sender, 
+                "Numero massi di etichette raggiunto, usa un address differente"
+            );
+        _;
+    }
+
+    modifier checkEmailState(EmailState expectedState, EmailState currentState) {
+        if(expectedState != currentState)
+            revert InvalidEmailState(
+                expectedState,
+                currentState,
+                "The current state is not the same as the expected state"
+            );
+        _;
+    }
+
+    modifier validSendAddress(address sender_address) {
+        if (msg.sender == sender_address)
+            revert NotValidAddress("");
+        _;
+    }
+
+    function nonRepudiationOfOrigin ( Signature nro, bytes32 proofToDo ) 
+        external 
+        override 
+        labelsAvailable 
+        checkEmailState(EmailState.NOT_USED, currentEmailState[msg.sender][currentLabel[msg.sender]].state)
+        returns(uint256 label) 
+    {
+        label = currentLabel[msg.sender]++;
+        currentEmailState[msg.sender][label].state = EmailState.NRO;
+        currentEmailState[msg.sender][label].proofToDo = proofToDo;
+
+        emit NonRepudiationOfOrigin( label, msg.sender, nro, block.timestamp);
+    }
+
+    function nonRepudiationOfReceipt ( Signature nrr, address sender_address, uint256 label, Hash proof ) 
+        external 
+        override 
+        validSendAddress(sender_address)
+        checkEmailState(EmailState.NRO, currentEmailState[sender_address][label].state)
+    {
+        bytes32 expectedHash = currentEmailState[sender_address][label].proofToDo;
+        bytes32 currentHash = keccak256(abi.encode(proof));
+
+        if ( currentHash != expectedHash)
+            revert NotValidHash(expectedHash, currentHash);
+
+        currentEmailState[sender_address][label].state = EmailState.NRR;
+        emit NonRepudiationOfReceipt( label, sender_address, nrr, block.timestamp);
+    }
+    
+    function submissionOfKey ( SymmetricKey key, Signature sub_k, uint256 label ) 
+        external 
+        override 
+        checkEmailState(EmailState.NRR, currentEmailState[msg.sender][currentLabel[msg.sender]].state)
+
+    {
+        currentEmailState[msg.sender][label].state = EmailState.CON_K;
+        delete currentEmailState[msg.sender][label].proofToDo;
+
+        emit SubmissionOfKey( label, msg.sender, key, sub_k, block.timestamp );
+    }
+
 }
--- contracts/IFairDelivery.sol
+++ contracts/IFairDelivery.sol
@@ -1,43 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract IFairDelivery {
-    // ** TYPES **
-    type HASH is uint256;
+interface IFairDelivery {
+    // -- TYPES --
+    type Hash is uint256;
+    type Signature is uint256;
+    type SymmetricKey is uint256;
+    type Ciphertext is uint256;
 
-    // ** EVENTS **
+    // -- ENUMS --
+    enum EmailState{ NOT_USED, NRO, NRR, CON_K }
 
-    event NonRepudiationOfOrigin (
-        address indexed transactionSigner, 
-        HASH emailFrom, HASH emailTo, 
-        uint indexed label, 
-        HASH indexed encMsg
-    );
-    
-    event NonRepudiationOfReceipt (
-        address indexed transactionSigner, 
-        HASH emailFrom, 
-        HASH emailTo, 
-        uint indexed label, 
-        HASH indexed encMsg
-    );
-    
-    event SubmissionOfKey (
-        address indexed transactionSigner, 
-        HASH emailFrom, 
-        HASH emailTo, 
-        uint indexed label, 
-        HASH indexed encKey
-    );
-    
-    event ConfirmationOfKey (
-        address indexed transactionSigner, 
-        HASH emailFrom, 
-        HASH emailTo, 
-        uint indexed label, 
-        HASH indexed encKey
-    );
+    // -- ERRORS --
+    error NotEnoughLabels (address addr, string description);
+    error InvalidEmailState (EmailState expectedState, EmailState currentState, string description);
+    error NotValidAddress (string description);
+    error NotValidHash (bytes32 expected, bytes32 current);
 
-    // ** FUNCTIONS **
+    // -- EVENTS --
+    event NonRepudiationOfOrigin (uint256 indexed label, address indexed sender_address, Signature nro, uint256 currentTimestamp);
+    event NonRepudiationOfReceipt (uint256 indexed label, address indexed sender_address, Signature nrr, uint256 currentTimestamp);
+    event SubmissionOfKey (uint256 indexed label, address indexed sender_address, SymmetricKey key, Signature sub_k, uint256 currentTimestamp);
+    // event ConfirmationOfKey ( );
+
+    // -- FUNCTIONS --
+    function currentLabel (address) external returns(uint256);
+    function nonRepudiationOfOrigin (Signature, bytes32) external returns(uint256 label);
+    function nonRepudiationOfReceipt (Signature, address, uint256, Hash) external;
+    function submissionOfKey (SymmetricKey, Signature, uint256) external;
 
 }
