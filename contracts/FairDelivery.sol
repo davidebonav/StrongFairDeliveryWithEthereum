48bd4714d3334490dc2addcefd0d72f3ea93df47
Remix Workspace <>
2022-10-07 20:18:06 +0200

    edit contract

--- contracts/FairDelivery.sol
+++ contracts/FairDelivery.sol
@@ -5,32 +5,38 @@ import "./Destructible.sol";
 import "./IFairDelivery.sol";
 import "./Payable.sol";
 
-contract FairDelivery is Payable, Destructible, IFairDelivery{
+contract FairDelivery is Payable, Destructible, IFairDelivery {
     // -- Types --
     struct StateData {
         bytes32 proofToDo;
-        EmailState state;
+        MessageState state;
     }
 
+    // -- Constant --
+    uint256 private constant MAX_INT = 2**256 - 1;
+    
     // -- Attributes --
-    uint256 private constant MAX_INT = 2**256-1;
-
+    /// @inheritdoc IFairDelivery
     mapping(address => uint256) public override currentLabel;
-    mapping(address => mapping(uint256 => StateData) ) private currentEmailState;
+    mapping(address => mapping(uint256 => StateData))
+        private currentMessageState;
 
     // -- Modifiers --
-    modifier labelsAvailable(){
+    modifier labelsAvailable {
         if (currentLabel[msg.sender] == MAX_INT)
             revert NotEnoughLabels(
-                msg.sender, 
-                "Numero massi di etichette raggiunto, usa un address differente"
+                msg.sender,
+                "Maximum number of labels for this address reached, use a different one"
             );
         _;
     }
 
-    modifier checkEmailState(EmailState expectedState, EmailState currentState) {
-        if(expectedState != currentState)
-            revert InvalidEmailState(
+    modifier checkMessageState(
+        MessageState expectedState,
+        MessageState currentState
+    ) {
+        if (expectedState != currentState)
+            revert InvalidMessageState(
                 expectedState,
                 currentState,
                 "The current state is not the same as the expected state"
@@ -38,57 +44,82 @@ contract FairDelivery is Payable, Destructible, IFairDelivery{
         _;
     }
 
-    modifier validSendAddress(address sender_address) {
-        if (msg.sender == sender_address)
-            revert NotValidAddress("");
+    modifier authorizedAddress(address sender_address, Hash proof, uint256 label) {
+        bytes32 expectedHash = currentMessageState[sender_address][label].proofToDo;
+        bytes32 currentHash = keccak256(abi.encode(proof));
+
+        if (currentHash != expectedHash)
+            revert UnauthorizedAddress(expectedHash, currentHash);
         _;
     }
 
-    constructor(uint256 minFee) Payable (minFee) {
-        
+    modifier validAddress {
+        if(msg.sender == address(0)){
+            revert NotValidAddress();
+        }
+        _;
     }
 
-    function nonRepudiationOfOrigin ( Signature nro, bytes32 proofToDo ) 
+    constructor(uint256 minFee) payable Payable(minFee) {}
+
+    /// @inheritdoc IFairDelivery
+    function nonRepudiationOfOrigin(Signature nro, bytes32 proofToDo)
+        external
         payable
-        external 
-        override 
-        labelsAvailable 
-        checkEmailState(EmailState.NOT_USED, currentEmailState[msg.sender][currentLabel[msg.sender]].state)
+        override
+        validAddress
+        labelsAvailable
+        checkMessageState(
+            MessageState.NULL_STATE,
+            currentMessageState[msg.sender][currentLabel[msg.sender]].state
+        )
         enoughFee
-        returns(uint256 label) 
+        returns (uint256 label)
     {
         label = currentLabel[msg.sender]++;
-        currentEmailState[msg.sender][label].state = EmailState.NRO;
-        currentEmailState[msg.sender][label].proofToDo = proofToDo;
+        currentMessageState[msg.sender][label].state = MessageState.NRO;
+        currentMessageState[msg.sender][label].proofToDo = proofToDo;
 
-        emit NonRepudiationOfOrigin( label, msg.sender, nro, block.timestamp);
+        emit NonRepudiationOfOrigin(label, msg.sender, nro);
     }
 
-    function nonRepudiationOfReceipt ( Signature nrr, address sender_address, uint256 label, Hash proof ) 
-        external 
-        override 
-        validSendAddress(sender_address)
-        checkEmailState(EmailState.NRO, currentEmailState[sender_address][label].state)
+    /// @inheritdoc IFairDelivery
+    function nonRepudiationOfReceipt(
+        Signature nrr,
+        address sender_address,
+        uint256 label,
+        Hash proof
+    )
+        external
+        override
+        validAddress
+        authorizedAddress(sender_address, proof, label)
+        checkMessageState(
+            MessageState.NRO,
+            currentMessageState[sender_address][label].state
+        )
     {
-        bytes32 expectedHash = currentEmailState[sender_address][label].proofToDo;
-        bytes32 currentHash = keccak256(abi.encode(proof));
-
-        if ( currentHash != expectedHash)
-            revert NotValidHash(expectedHash, currentHash);
-
-        currentEmailState[sender_address][label].state = EmailState.NRR;
-        emit NonRepudiationOfReceipt( label, sender_address, nrr, block.timestamp);
+        currentMessageState[sender_address][label].state = MessageState.NRR;
+        emit NonRepudiationOfReceipt(label, sender_address, nrr);
     }
-    
-    function submissionOfKey ( SymmetricKey key, Signature sub_k, uint256 label ) 
-        external 
-        override 
-        checkEmailState(EmailState.NRR, currentEmailState[msg.sender][currentLabel[msg.sender]].state)
+
+    /// @inheritdoc IFairDelivery
+    function submissionOfKey(
+        SymmetricKey key,
+        Signature sub_k,
+        uint256 label
+    )
+        external
+        override
+        validAddress
+        checkMessageState(
+            MessageState.NRR,
+            currentMessageState[msg.sender][currentLabel[msg.sender]].state
+        )
     {
-        currentEmailState[msg.sender][label].state = EmailState.CON_K;
-        delete currentEmailState[msg.sender][label].proofToDo;
+        currentMessageState[msg.sender][label].state = MessageState.CON_K;
+        delete currentMessageState[msg.sender][label].proofToDo;
 
-        emit SubmissionOfKey( label, msg.sender, key, sub_k, block.timestamp );
+        emit SubmissionOfKey(label, msg.sender, key, sub_k);
     }
-
 }
--- contracts/IFairDelivery.sol
+++ contracts/IFairDelivery.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.17;
 
 interface IFairDelivery {
+    /**
+     * @title A Smart Contract for fair strong delivery
+     * @author Davide Bonaventura
+     * @notice This Smart Contract allows you to obtain non-repudiation property for all messages exchanged using any asynchronous messaging service
+     */
+
     // -- TYPES --
     type Hash is uint256;
     type Signature is uint256;
@@ -9,24 +15,81 @@ interface IFairDelivery {
     type Ciphertext is uint256;
 
     // -- ENUMS --
-    enum EmailState{ NOT_USED, NRO, NRR, CON_K }
+    /// @notice Questo enum contiene tutti gli stati ammessi dallo Smart Contract per un qualunque messaggio
+    enum MessageState {
+        NULL_STATE,
+        NRO,
+        NRR,
+        CON_K
+    }
 
     // -- ERRORS --
-    error NotEnoughLabels (address addr, string description);
-    error InvalidEmailState (EmailState expectedState, EmailState currentState, string description);
-    error NotValidAddress (string description);
-    error NotValidHash (bytes32 expected, bytes32 current);
+    error NotEnoughLabels(address addr, string description);
+    error InvalidMessageState(
+        MessageState expectedState,
+        MessageState currentState,
+        string description
+    );
+    error UnauthorizedAddress(bytes32 expected, bytes32 current);
+    error NotValidAddress();
 
     // -- EVENTS --
-    event NonRepudiationOfOrigin (uint256 indexed label, address indexed sender_address, Signature nro, uint256 currentTimestamp);
-    event NonRepudiationOfReceipt (uint256 indexed label, address indexed sender_address, Signature nrr, uint256 currentTimestamp);
-    event SubmissionOfKey (uint256 indexed label, address indexed sender_address, SymmetricKey key, Signature sub_k, uint256 currentTimestamp);
-    // event ConfirmationOfKey ( );
+    /// @notice This event is placed whenever the sender intends to provide the recipient with evidence of the authenticity of the message received
+    event NonRepudiationOfOrigin(
+        uint256 indexed label,
+        address indexed sender_address,
+        Signature nro
+    );
+
+    /// @notice This event is placed every time the recipient wants to confirm that they have received the cryptotext and want to proceed to obtain the key to decrypt it
+    event NonRepudiationOfReceipt(
+        uint256 indexed label,
+        address indexed sender_address,
+        Signature nrr
+    );
+
+    /// @notice This event is set whenever the sender wants to send the key to the recipient to decrypt the message
+    event SubmissionOfKey(
+        uint256 indexed label,
+        address indexed sender_address,
+        SymmetricKey key,
+        Signature sub_k
+    );
 
     // -- FUNCTIONS --
-    function currentLabel (address) external returns(uint256);
-    function nonRepudiationOfOrigin (Signature, bytes32) payable external returns(uint256 label);
-    function nonRepudiationOfReceipt (Signature, address, uint256, Hash) external;
-    function submissionOfKey (SymmetricKey, Signature, uint256) external;
+    /// @notice This method allows you to get the next label for any address
+    /// @param _addr address of which you want to get the next label
+    /// @return Next label
+    function currentLabel(address _addr) external returns (uint256);
 
+    /// @notice This method allows the sender to publish the non-repudiation of origin flag relative to a message
+    /// @param nro the evidence to publish
+    /// @param proofToDo a value that only the correct recipient can calculate
+    /// @return Returns the label to which the evidence was associated
+    function nonRepudiationOfOrigin(Signature nro, bytes32 proofToDo)
+        external
+        payable
+        returns (uint256);
+
+    /// @notice This method allows the recipient to communicate that they have received the message and want to obtain the key to decrypt it
+    /// @param nrr the evidence to publish
+    /// @param sender_address the address from which the previous evidence was published
+    /// @param label the label with which the evidence relating to the message in question is associated
+    /// @param proof a hash that allows you to prove that you are the real recipient of the message
+    function nonRepudiationOfReceipt(
+        Signature nrr,
+        address sender_address,
+        uint256 label,
+        Hash proof
+    ) external;
+
+    /// @notice This method allows the recipient to publish the key that decrypts the message
+    /// @param key symmetric key with which the message can be decrypted
+    /// @param sub_k evidence that guarantees that the published key is the correct one
+    /// @param label the label that allows you to identify the message to which the evidence and the key refer
+    function submissionOfKey(
+        SymmetricKey key,
+        Signature sub_k,
+        uint256 label
+    ) external;
 }
